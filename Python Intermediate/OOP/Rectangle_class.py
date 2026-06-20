d3aaf242851fbef826ff7c4f6ee65a88500a6b37
Dominick <guevaradominick2@gmail.com>
2026-06-20 02:22:24 -0600

    Rectangle_class corrected

--- Python Intermediate/OOP/Rectangle_class.py
+++ Python Intermediate/OOP/Rectangle_class.py
@@ -1,28 +1,18 @@
-class Rectangle():
-    def __init__(self, width=0, height=0):
+class Rectangle:
+    def __init__(self, width, height):
+        if width < 0 or height < 0:
+            raise ValueError("Width and height cannot be negative")
         self.width = width
         self.height = height
 
     def get_area(self):
-        while True:
-            try:
-                self.height = int(input('Insert rectangle height: '))
-                self.width = int(input('Insert rectangle width: '))
-                if self.height < 0 or self.width < 0:
-                        raise ValueError
-            except ValueError as ex:
-                print(f"Error - Text or negative values are not allowed")
-                continue
-                
-            return self.width * self.height
-        
-    
+        return self.width * self.height
+
     def get_perimeter(self):
-        perimeter = (2 * self.width) + (2 * self.height)
-        return perimeter
+        return (2 * self.width) + (2 * self.height)
 
 
-r_shape = Rectangle()
-print(r_shape.get_area())
-print(r_shape.get_perimeter())
+r_shape = Rectangle(5, 3)
+print(r_shape.get_area())       # 15
+print(r_shape.get_perimeter())  # 16
 
